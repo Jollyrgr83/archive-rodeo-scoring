20270a7e72e348dd1b861dd8ed84c6698a193140
jollyrgr83 <jasongmangin@gmail.com>
2020-06-22 16:22:12 -0600

    style update

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
 const mysql = require("mysql");
 const connObj = { host: "localhost", user: "root", password: "", database: "rodeo" };
-let connection;
-process.env.JAWSDB_URL ? connection = mysql.createConnection(process.env.JAWSDB_URL) : connection = mysql.createConnection(connObj);
+let connection; 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);  
+} else {
+  connection = mysql.createConnection(connObj);
+}
 connection.connect(err => {
   if (err) {
     console.error("error connecting: " + err.stack);
